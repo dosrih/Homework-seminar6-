588b23255cad04dd0e28a435a9b9eebe86cf9233
Doston <119062575+dosrih@users.noreply.github.com>
2022-12-21 18:30:52 +0500

    Add files via upload

---
+++ hom_c2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Intersection at a point: ({x};{y})");
